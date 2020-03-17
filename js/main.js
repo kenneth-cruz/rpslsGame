b22e8b72f62d973d852ed89a3c81ddec78af8501
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-03-17 11:24:14 -0400

    First Verstion of RPSLS game

---
+++ js/main.js
@@ -0,0 +1,68 @@
+let arrayOptions = ['rock','paper','scissors','lizzard','spock']
+let randomOption = Math.round(Math.random()*4)
+let computerRandomMove = arrayOptions[randomOption].toString()
+
+// Rock crushes lizard.
+// Rock crushes scissors.
+document.getElementById('rock').addEventListener('click', function(){
+  document.getElementById('computerMove').innerHTML = computerRandomMove
+  if(computerRandomMove === 'scissors' || computerRandomMove === 'lizzard'){
+    document.getElementById('result').innerHTML = "You WIN !"
+  }
+  else if(computerRandomMove ==='rock'){
+    document.getElementById('result').innerHTML = "You Tied :|"
+  }
+  else{ document.getElementById('result').innerHTML = "You lost :( " }
+})
+//paper
+// Paper covers rock.
+// Paper disproves Spock.
+document.getElementById('paper').addEventListener('click', function(){
+  document.getElementById('computerMove').innerHTML = computerRandomMove
+  if(computerRandomMove === 'rock' || computerRandomMove === 'spock'){
+    document.getElementById('result').innerHTML = "You WIN !"
+  }
+  else if(computerRandomMove ==='paper'){
+    document.getElementById('result').innerHTML = "You Tied :|"
+  }
+  else{ document.getElementById('result').innerHTML = "You lost :( " }
+})
+//scissors
+// Scissors cuts paper.
+// Scissors decapitates lizard.
+document.getElementById('scissors').addEventListener('click', function(){
+  document.getElementById('computerMove').innerHTML = computerRandomMove
+  if(computerRandomMove === 'paper' || computerRandomMove === 'lizzard'){
+    document.getElementById('result').innerHTML = "You WIN !"
+  }
+  else if(computerRandomMove ==='scissors'){
+    document.getElementById('result').innerHTML = "You Tied :|"
+  }
+  else{ document.getElementById('result').innerHTML = "You lost :( " }
+})
+//lizzard
+// Lizard poisons Spock.
+// Lizard eats paper.
+document.getElementById('lizzard').addEventListener('click', function(){
+  document.getElementById('computerMove').innerHTML = computerRandomMove
+  if(computerRandomMove === 'spock' || computerRandomMove === 'paper'){
+    document.getElementById('result').innerHTML = "You WIN !"
+  }
+  else if(computerRandomMove ==='lizzard'){
+    document.getElementById('result').innerHTML = "You Tied :|"
+  }
+  else{ document.getElementById('result').innerHTML = "You lost :( " }
+})
+//spock
+// Spock smashes scissors.
+// Spock vaporizes rock.
+document.getElementById('spock').addEventListener('click', function(){
+  document.getElementById('computerMove').innerHTML = computerRandomMove
+  if(computerRandomMove === 'scissors' || computerRandomMove === 'rock'){
+    document.getElementById('result').innerHTML = "You WIN !"
+  }
+  else if(computerRandomMove ==='spock'){
+    document.getElementById('result').innerHTML = "You Tied :|"
+  }
+  else{ document.getElementById('result').innerHTML = "You lost :( " }
+})
